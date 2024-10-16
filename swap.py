c9256c812e2985e613265e405b18edb9a18b35d1
rehaab fatima <rehaabfatima628@gmail.comm>
2024-10-16 20:50:54 +0500

    add swap

---
+++ swap.py
@@ -0,0 +1,18 @@
+x=input("Enter first number")
+y=input("Enter second number")
+z=input("Enter second number")
+
+temp=x
+x=y
+y=z
+z=x
+z=temp
+
+print("The number you entered is",x)
+print ("The number you entered then is",y)
+print("The number you entered last is",z)
+
+
+
+
+
